cb384afa79dab29b184f2213870c4708bc910d6d
santigarciab <108765719+santigarciab@users.noreply.github.com>
2022-07-06 11:16:08 -0300

    Update Actualizar.sh

--- Actualizar.sh
+++ Actualizar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt autoremove
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade && sudo apt autoremove
